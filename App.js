f9840405cbd327923e10a75fc765eac481dcbd5b
Saswata Mukherjee <saswataminsta@yahoo.com>
2020-06-18 23:34:48 +0530

    Added cards

--- App.js
+++ App.js
@@ -1,17 +1,100 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { 
+  Text, 
+  View,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Animated,
+  FlatList,
+} from 'react-native';
+import SlidingUpPanel from 'rn-sliding-up-panel';
+import Carousel from 'react-native-snap-carousel';
+import {MaterialIcons} from '@expo/vector-icons';
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
 
 export default function App() {
+
+  const Images = [
+    {
+      image: require('./assets/card1.png'),
+    },
+    {
+      image: require('./assets/card2.png'),
+    },
+    {
+      image: require('./assets/card3.png'),
+    },
+    {
+      image: require('./assets/card4.png'),
+    },
+    {
+      image: require('./assets/card5.png'),
+    },
+    {
+      image: require('./assets/card6.png'),
+    },
+  ];
+
+  const {width, height} = Dimensions.get('window');
+  const carouselRef = useRef(null);
+
+  const RenderItem = ({item}) => {
+    return(
+      <TouchableWithoutFeedback>
+        <Image source={item.image} style={{height: 240, width: 360, borderRadius: 10}}/>
+      </TouchableWithoutFeedback>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{paddingTop: 50, paddingHorizontal: 14}}>
+
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+          <View>
+            <Text style={{fontSize: 26, color: '#fff'}}>Welcome Back,</Text>
+            <Text style={{fontSize: 26, color: '#fff', opacity: 0.6}}>Saswata Mukherjee</Text>
+          </View>
+          <View>
+            <Image 
+            source={{uri:
+            'https://images.pexels.com/photos/936229/pexels-photo-936229.jpeg?auto=compress&cs=tinysrgb&h750&w=1260'
+            }}
+            style={styles.ProfileImage} 
+            />
+            <View style={styles.ProfileImageNotification}></View>
+          </View>
+        </View>
+
+        <View>
+          <Carousel
+            layout={"tinder"}
+            ref={carouselRef}
+            renderItem = {RenderItem}
+            data={Images}
+            sliderWidth={width}
+            itemWidth={width - 10}
+            swipeThreshold={100}
+            layoutCardOffset={-12}
+            inactiveSlideOpacity={0.4}
+            containerCustomStyle={{
+              overflow: 'visible',
+              marginVertical: 30
+            }}
+            contentContainerCustomStyle={{
+              paddingTop: 14
+            }}
+          />
+        </View>
+
+        <View>
+          <Text style={{color: "#fff", opacity: 0.6, marginBottom: 10}}>Send Money</Text>
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -19,18 +102,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#000',
+    paddingTop: 0
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  ProfileImage: {
+    width: 55,
+    height: 55,
+    borderRadius: 40
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  ProfileImageNotification: {
+    height: 18,
+    width: 18,
+    borderRadius: 9,
+    backgroundColor: '#4853ef',
+    right: 0,
+    position: 'absolute',
+    borderWidth: 2,
+    borderColor: '#000'
+  }
+})
